60957df42c62e433ccf5cc4f2cd823074a51d088
Diego Feversani <diego.feversani@gmail.com>
2021-10-28 10:27:59 -0300

    Se agrego la explicacion de lambda

---
+++ lambda.py
@@ -0,0 +1,12 @@
+palindrome = lambda string: string == string[::-1]
+
+def run():
+    word = input("Ingrese una palabra: ")
+    if palindrome(word):
+        print("Es palindromo!")
+    else:
+        print("No es palindromo!")
+
+
+if __name__ == "__main__":
+    run()
